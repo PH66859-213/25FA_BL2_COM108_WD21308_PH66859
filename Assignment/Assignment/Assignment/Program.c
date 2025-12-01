4ae45d8f4a67084c3946c7667201f5a230c5b136
Nguyen Dinh Khanh <ndkhanhit23@gmail.com>
2025-12-01 16:55:46 +0700

    CN1-2-3 Assignment

--- Assignment/Assignment/Assignment/Program.c
+++ Assignment/Assignment/Assignment/Program.c
@@ -5,6 +5,7 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
 
 
 void clear_input_buffer() {
@@ -14,80 +15,108 @@ void clear_input_buffer() {
 
 typedef void (*FunctionPtr)();
 
-void handle_function_loop(FunctionPtr function_to_call/*, const char* function_name*/) {
-
-    int tieptuc;
-    int luaChonHopLe;
-
-    do
-    {
-        function_to_call();
-
-        do
-        {
-            luaChonHopLe = 1;
-            printf("\n------------------------------\n");
-            printf("Ban co muon TIEP TUC thuc hien Chuc Nang nay hay khong?\n");
-            printf("Nhap [1] de tiep tuc, nhap [0] de thoat ve Menu chinh: \n");
-
-            if (scanf_s("%d", &tieptuc) != 1) {
-                printf("Lua chon khong hop le (Nhap sai kieu du lieu)! Vui long nhap [1] hoac [0]!\n");
-                luaChonHopLe = 0;
-            }
-            else if (tieptuc != 1 && tieptuc != 0) {
-                printf("Lua chon khong hop le!! Vui long nhap [1] hoac [0]!\n");
-                luaChonHopLe = 0;
-            }
-            clear_input_buffer();
-        } while (luaChonHopLe == 0);
-    } while (tieptuc == 1);
-}
-
 void CN1_KiemTraSoNguyen() {
-
+    int x;
+    int isPrime=0;
+    int isSquare;
     printf("\n===================================\n");
     printf("Chuc nang so 1 : Kiem tra 1 so!\n");
+    while (scanf_s("%d", &x) != 1) {
+        printf("Du lieu khong hop le! Xin nhap lai 1 so nguyen");
+    }
+    printf("So %d la mot so nguyen! \n", x);
+    if (x < 3) {
+        printf("So %d khong phai la so nguyen to!!\n", x);
+    }
+    for (int i = 2; i < x; i++) {
+        if (x % i == 0) {
+            isPrime++;
+        }
+    }
+    if (isPrime == 0) {
+        printf("So %d la so nguyen to!\n",x);
+
+    }
+    else {
+        printf("So %d khong phai so nguyen to!\n",x);
+
+    }
+    isSquare = (int)sqrt(x);
+    if (pow(isSquare, 2) == x) {
+        printf("So %d la so chinh phuong !\n", x);
+    }
+    else {
+        printf("So %d khong phai la so chinh phuong !\n", x);
+    }
     printf("\n===================================\n");
 
 
+}
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int lcm(int a, int b) {
+    int u = gcd(a, b);
+    return (a / u) * b;
 }
 void CN2_TimUocVaBoi() {
-    //printf("Day la chuc nang so 2 : Tim UCLN va BCNN\n");
-    //int tieptuc = 1;
-    /*do
-    {*/
+    int x;
+    int y;
     printf("\n===================================\n");
     printf("Chuc nang so 2 : Tim UCLN va BCNN\n");
+    printf("Nhap vao 2 so nguyen bat ky: ");
+    scanf_s("%d", &x);
+    scanf_s("%d", &y);
+    printf("Uoc chung lon nhat cua %d va %d la: %d\n", x, y, gcd(x, y));
+    printf("Boi chung nho nhat cua %d va %d la: %d\n", x, y, lcm(x, y));
     printf("\n===================================\n");
-    //int soNguyen;
-    //printf("\n----------------------------------\n");
-    /*printf("Ban co muon TIEP TUC thuc hien Chuc Nang 2 khong?\n");
-    printf("Nhap [1] de tiep tuc, nhap [0] de thoat ve Menu chinh: ");
-
-    scanf_s("%d", &tieptuc);
-    if (tieptuc != 1 && tieptuc != 0) {
-        printf("Lua chon khong hop le!! Vui long nhap [1] hoac [0]!");
-    }*/
-    /*} while (tieptuc == 1);*/
+   
 }
+
 void CN3_TinhTienKaraoke() {
-    //printf("Day la chuc nang so 3 : Tinh tien cho quan Karaoke\n");
-    /*int tieptuc = 1;
-    do
-    {*/
+    int gioVao;
+    int gioRa;
+    double bill;
     printf("\n===================================\n");
     printf("Chuc nang so 3 : Tinh tien cho quan Karaoke\n");
-    printf("\n===================================\n");
-    //int soNguyen;
-    //printf("\n----------------------------------\n");
-//    printf("Ban co muon TIEP TUC thuc hien Chuc Nang 3 khong?\n");
-//    printf("Nhap [1] de tiep tuc, nhap [0] de thoat ve Menu chinh: ");
+    printf("Nhap gio vao quan: ");
+    while (scanf_s("%d", &gioVao) != 1) {
+        printf("Du lieu khong hop le, vui long nhap lai!!! :");
+    }
+    if (gioVao < 12 || gioVao >23) {
+        printf("Quan chi mo cua tu 12h - 23h! Vui long quay lai sau!!\n");
+        return;
+    }
+    printf("Nhap gio ra quan: ");
+    while (scanf_s("%d", &gioRa) != 1) {
+        printf("Du lieu khong hop le, vui long nhap lai!!! :");
+    }
+    if (gioRa <= gioVao || gioRa > 23) {
+        printf("Gio ra phai lon hon gio vao va <= 23h!\n");
+        return;
+    }
+    int tongGio = gioRa - gioVao;
+    
+    if (tongGio < 4) {
+        bill = tongGio * 150000;
+    }
+    else {
+        bill = 3 * 150000 + ((tongGio - 3) * 150000 * 0.7);
+    }
+    if (gioVao >= 12 && gioVao<=17) {
+        bill = bill * 0.9;
+    }
 
-//    scanf_s("%d", &tieptuc);
-//    if (tieptuc != 1 && tieptuc != 0) {
-//        printf("Lua chon khong hop le!! Vui long nhap [1] hoac [0]!");
-//    }
-//} while (tieptuc == 1);
+    printf("Tong so tien can thanh toan cua ban la: %.2lf VND \n", bill);
+
+    printf("\n===================================\n");
+    
 }
 void CN4_TinhTienDien() {
     //printf("Day la chuc nang so 4 : Tinh tien dien!\n");
@@ -263,9 +292,50 @@ void sapXepPhanTuMang1Chieu() {
 
 }
 
+void lapChucNang(int chonChucNang) {
+    int tiepTuc = 1;
+    while (tiepTuc == 1) {
+        switch (chonChucNang)
+        {
+            case 0: printf("\n Cam on quy khach da su dung dich vu!!\n");
+                return;
+            case 1: CN1_KiemTraSoNguyen();
+                break;
+            case 2: CN2_TimUocVaBoi();
+                break;
+            case 3: CN3_TinhTienKaraoke();
+                break;
+            //case 4: CN4_TinhTienDien();
+            //    break;
+            //case 5: CN5_DoiTien();
+            //    break;
+            //case 6: handle_function_loop(CN6_TinhLaiSuat);
+            //    break;
+            //case 7: handle_function_loop(CN7_VayTienMuaXe);
+            //    break;
+            //case 8: handle_function_loop(CN8_SapXepThongTinSinhVien);
+            //    break;
+            //case 9: handle_function_loop(CN9_GameFPOLY_LOTT);
+            //    break;
+            //case 10: handle_function_loop(CN10_TinhToanPhanSo);
+            //    break;
+            //case 11: handle_function_loop(sapXepPhanTuMang1Chieu);
+            //    break;
+            default:
+                printf("Chuc nang khong hop le!! Vui long chon chuc nang tu [1-10]! : ");
+                break;
+        }
+        
+            printf("Tiep tuc chuc nang? [1 = Co | 0 = Khong ] !!");
+            scanf_s("%d", &tiepTuc);
+            system("cls");
+    }
+
+}
+
 int main() {
 
-    int chon;
+    int chonChucNang;
     do
     {
         printf("==============Menu============\n");
@@ -283,37 +353,17 @@ int main() {
         printf("0.  THOAT KHOI UNG DUNG\n");
         printf("=============================");
         printf("\nChon Chuc Nang Tren Menu: ");
-        scanf_s("%d", &chon);
-        switch (chon) {
-        case 0: printf("\n Cam on quy khach da su dung dich vu!!\n");
-            break;
-        case 1: handle_function_loop(CN1_KiemTraSoNguyen);
-            break;
-        case 2: handle_function_loop(CN2_TimUocVaBoi);
-            break;
-        case 3: handle_function_loop(CN3_TinhTienKaraoke);
-            break;
-        case 4: handle_function_loop(CN4_TinhTienDien);
-            break;
-        case 5: handle_function_loop(CN5_DoiTien);
-            break;
-        case 6: handle_function_loop(CN6_TinhLaiSuat);
-            break;
-        case 7: handle_function_loop(CN7_VayTienMuaXe);
-            break;
-        case 8: handle_function_loop(CN8_SapXepThongTinSinhVien);
-            break;
-        case 9: handle_function_loop(CN9_GameFPOLY_LOTT);
-            break;
-        case 10: handle_function_loop(CN10_TinhToanPhanSo);
-            break;
-        case 11: handle_function_loop(sapXepPhanTuMang1Chieu);
-            break;
-        default:
-            printf("Chuc nang khong hop le!! Vui long chon chuc nang tu [1-10]!");
-            break;
+        scanf_s("%d", &chonChucNang);
+        if (chonChucNang < 0 || chonChucNang>10) {
+            printf("Khong hop le! Vui long chon chuc nang tu [1-10]: ");
+            scanf_s("%d", &chonChucNang);
+            lapChucNang(chonChucNang);
         }
-    } while (chon != 0);
+        else {
+            lapChucNang(chonChucNang);
+
+        }
+    } while (chonChucNang != 0);
 
 
 }
